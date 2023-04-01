94c54acdc88dc38382194c1785eb9fe89fe35bb4
yashPundhir <yash.pundhir.prof@gmail.com>
2023-04-01 13:24:26 +0530

    Modal color scheme updated & req components added to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import TotalExpense from "./components/TotalExpense";
 
 function App() {
 	return (
-		<div>
-			<h1>Hello world</h1>
+		<div className="flex flex-col justify-center items-center">
+			<ExpenseForm />
+			<h1 className="mt-16 text-[45px] font-semibold">My Expenses</h1>
+			<ExpenseList />
+			<TotalExpense />
 		</div>
 	);
 }
